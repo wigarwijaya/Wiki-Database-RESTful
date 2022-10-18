91b1ed3c334d2e06e9e682469e158bdb78beb82e
wigarwijaya <wigarwijaya@gmail.com>
2022-10-18 22:19:45 +0700

    create RESTful

--- app.js
+++ app.js
@@ -1,55 +1,129 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const port = 3000;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(urlencoded({extended: true}))
-app.use(express.static('public'));
+app.use(urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //  ----------------------------------------------------------------------
 // setup mongoose schema & create collections
 
-mongoose.connect('mongodb://localhost:27017/wikiDB');
+mongoose.connect("mongodb://localhost:27017/wikiDB");
 const { Schema } = mongoose;
 
-const articleSchema = new Schema ({
-    title: String,
-    content: String
-})
+const articleSchema = new Schema({
+  title: String,
+  content: String,
+});
 
 const Article = mongoose.model("Article", articleSchema);
 
-// const article1 = new Article ({
-//     title:
-//     content:
-// })
+// TODO
+//  ----------------------------------------------------------------------
+// Create "/" Route
+
+app.route("/").get((req, res) => {
+  Article.find({}, (err, docs) => {
+    res.render("home", { documents: docs });
+  });
+});
 
 //  ----------------------------------------------------------------------
-// TODO
+// Create "/articles" Route
 
-app.get('/', (req, res) => {
+app
+  .route("/articles")
+  .get((req, res) => {
     Article.find({}, (err, docs) => {
-        res.render('home', { documents: docs });
-    })
-})
+      if (!err) {
+        res.send(docs);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    console.log(req.body.title);
+    console.log(req.body.content);
 
-app.get('/articles', (req, res) => {
-    Article.find({}, (err, docs) => {
-        if(!err){
-            res.send(docs)
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save((err) => {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+//  ----------------------------------------------------------------------
+// Create "/articles/:articleTitle" Route
+
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    Article.findOne({ title: req.params.articleTitle }, (err, foundArticle) => {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found!");
+      }
+    });
+  })
+  .put((req, res) => {
+    Article.replaceOne(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      (err) => {
+        if (!err) {
+          res.send("Successfully update the selected article");
         } else {
-            res.send(err)
+          res.send(err);
         }
-    })
-})
+      }
+    );
+  })
+  .patch((req, res) =>{
+    Article.updateOne(
+        { title: req.params.articleTitle },
+        { $set: req.body },
+        (err) => {
+          if (!err) {
+            res.send("Successfully updated the article");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+  })
+  .delete((req, res) =>{
+    Article.deleteOne(
+        { title: req.params.articleTitle }
+    ).then(() => res.send("Successfully delete the selected article"))
+    .catch(() => res.send(err))
+  });
+
+//  ----------------------------------------------------------------------
+// PORT
 
 app.listen(port, (req, res) => {
-    console.log("app is running on port " + port);
-})
-
-
+  console.log("app is running on port " + port);
+});
